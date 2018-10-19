9e638c5e979ad71d6d22d157a432a7816cdd375e
Loulan <yxqpcphone@163.com>
2018-10-19 18:29:51 +0800

    方正提交

--- Shopping-Web-Manager/src/main/webapp/js/controller/typeTemplateController.js
+++ Shopping-Web-Manager/src/main/webapp/js/controller/typeTemplateController.js
@@ -5,4 +5,38 @@ app.controller("typeTemplateController",function ($scope, $controller,typeTempla
 
     $scope.searchEntity = {name:""};
 
+    //获取关联想的名称属性
+    $scope.jsonToString = function (str, field) {
+        var jsons = JSON.parse(str);
+        var text = "";
+
+        for (var i=0;i<jsons.length;i++) {
+            if (i==jsons.length-1) {
+                text += jsons[i][field];
+            }
+            else
+            {
+                text += jsons[i][field]+",";
+            }
+        }
+        return text;
+    }
+
+    //对象数据进行保存
+    $scope.save = function () {
+        
+    }
+
+    //点击修改按钮
+    $scope.findOne = function (id) {
+        // var jsons = JSON.parse($scope.entity);
+        console.info($scope.list.length)
+        for (var i=0;i<$scope.list.length;i++) {
+            if ($scope.list[i].id == id) {
+                $scope.entity = $scope.list[i];
+                $scope.brandList = JSON.parse(str$scope.list[i].brandIds);
+            }
+        }
+    }
+
 });
